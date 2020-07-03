1e07ec8272fdb49caf3047b07a8654d9b22b8a43
Stephenhua <04121390@cumt.edu.cn>
2020-07-03 19:00:34 +0800

    valid date

--- shell/valid-date.sh
+++ shell/valid-date.sh
@@ -5,21 +5,20 @@ PATH=.:$PATH
 
 exceedsDaysInMonth()
 {
-    case $(echo $1|tr '[:upper]' '[:lower:]') in 
-        jan* ) days=31   ;;   feb* ) days=28  ;;
-        mar* ) days=31   ;;   apr* ) days=30  ;;
-        may* ) days=31   ;;   jun* ) days=30  ;;
-        jul* ) days=31   ;;   aug* ) days=31  ;;
-        sep* ) days=30   ;;   oct* ) days=31  ;;
-        nov* ) days=30   ;;   dec* ) days=31  ;;
-        * ) echo "$0:unknown month name $1" >&2 ; exit 1
-    esac
-
-    if [ $2 -lt 1 -o $2 -gt $days ] ; then
-        return 1
-    else
-        return 0
-    fi
+  case $(echo $1|tr '[:upper:]' '[:lower:]') in
+    jan* ) days=31    ;;  feb* ) days=28    ;;
+    mar* ) days=31    ;;  apr* ) days=30    ;;
+    may* ) days=31    ;;  jun* ) days=30    ;;
+    jul* ) days=31    ;;  aug* ) days=31    ;;
+    sep* ) days=30    ;;  oct* ) days=31    ;;
+    nov* ) days=30    ;;  dec* ) days=31    ;;
+    * ) echo "$0: Unknown month name $1" >&2; exit 1
+  esac
+   if [ $2 -lt 1 -o $2 -gt $days ] ; then
+     return 1
+   else
+     return 0   # the day number is valid 
+   fi 
 }
 
 isLeapYear()
@@ -48,29 +47,30 @@ fi
 
 #规范日期，保存返回值以供错误检查
 
-newdate="$($normdate "#@")"
-
+newdate="$(normdate "$@")"
+echo $newdate
 if [ $? -eq 1 ] ; then
     exit 1
 fi
 
 
 month="$(echo $newdate | cut -d\  -f1)"
-day="$(echo $newdate | cut -d\ -f2)"
-year="$(echo $newdate | cut -d\ -f3)"
-
+day="$(echo $newdate | cut -d\  -f2)"
+year="$(echo $newdate | cut -d\  -f3)"
 
 if ! exceedsDaysInMonth $month "$2" ; then
-    if [ "$month" = "Feb" -a "$2" -eq "29"] ; then
-        if ! isLeapYear $3 ; then
-            echo "$0:$3 is not a leap year ,so Feb doesn't have 29 days." >&2
-            exit 1
-        fi
-    else
-        echo "$0:bad day value:$month doesn't have $2 days" >&2
-        exit1
+  if [ "$month" = "Feb" -a "$2" -eq "29" ] ; then
+    if ! isLeapYear $3 ; then
+      echo "$0: $3 is not a leap year, so Feb doesn't have 29 days" >&2
+      exit 1
     fi
+  else 
+    echo "$0: bad day value: $month doesn't have $2 days" >&2
+    exit 1
+  fi
 fi
+
+
 
 echo "Valid date: $newdate"
 exit 0
